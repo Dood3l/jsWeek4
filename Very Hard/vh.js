bd44c7d40e2850c567700a8f04419e97aa1e97eb
Brian <56368937+Dood3l@users.noreply.github.com>
2022-02-01 18:35:58 -0500

    Update vh.js

--- Very Hard/vh.js
+++ Very Hard/vh.js
@@ -9,21 +9,45 @@
 // Output: -1
 // Note: You may assume that you have an infinite number of each kind of coin.
 
+//NEW LOGIC ↓
+//Create an array for the value of the coins, along with a variable for the total amount of money.
+//Create a function, probably for loop, that can take in the coin values and sort them from greatest to least.
+//Create a function that can see how many times a coin value can go into the total amount of money without a remainder. If a remainder exists, move onto the next coin value until no remainder.
+//If no remainder is found, return the amount of coins used.
+//If coin values cant equate to total amount of money, then return -1.
+
+let coinValues = [1, 5, 2];
+let money = 11;
+let numOfCoins = 0;
+
+coinValues.sort(function(a, b) {
+  return b - a;
+});
+console.log(coinValues);
+
+function coinCounter ( ){
+    for ( i = 0; i < coinValues.length; i++){
+        
+    }
+
+
+
+// FLAWED LOGIC ↓ 
 // Start with a const that contains the total amount of money. Then make variables for coins.
-let coins = [1, 2, 5];
-let total = 11
-//Create a for loop that gets sum of the array
-let sum = 0
-for (let i = 0; i < coins.length; i++) {
-    sum += coins[i];
-}
-// Create a function that uses if, else if, for the coins, and else for -1.
-function coinCounter(arr){
-    if (sum <= total){
-        return (total -= sum)
-    }
-    else if (sum != total){
-        return "-1"
-    }
-}
-console.log(coinCounter(sum));
+// let coins = [1, 2, 5];
+// let total = 11
+// //Create a for loop that gets sum of the array
+// let sum = 0
+// for (let i = 0; i < coins.length; i++) {
+//     sum += coins[i];
+// }
+// // Create a function that uses if, else if, for the coins, and else for -1.
+// function coinCounter(arr){
+//     if (sum <= total){
+//         return (total -= sum)
+//     }
+//     else if (sum != total){
+//         return "-1"
+//     }
+// }
+// console.log(coinCounter(sum));
